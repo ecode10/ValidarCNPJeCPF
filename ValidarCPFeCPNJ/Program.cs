3527358a939af9b5d9cd0f5907f0c693cacf5a89
Mauricio Junior <mauricio.junior@gmail.com>
2021-11-07 12:54:09 -0600

    adicionando projeto

---
+++ ValidarCPFeCPNJ/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ValidarCPFeCPNJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CPFCNPJ.IMain checkCPFCNPJ = new CPFCNPJ.Main();
+            var resultCNPJ = checkCPFCNPJ.IsValidCPFCNPJ("53645180000150");
+
+            Console.WriteLine(resultCNPJ);
+
+            var resultCPF = checkCPFCNPJ.IsValidCPFCNPJ("09033909922");
+            Console.WriteLine(resultCPF);
+        }
+    }
+}
